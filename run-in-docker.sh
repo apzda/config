6483499a10d674064945ec32ccad38e01689cb35
ninggf <windywany@163.com>
2024-04-05 23:15:31 +0800

    允许跳过单元测试

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -4,11 +4,16 @@ export SERVICE_NAME=config
 export SERVICE_VER=1.0.7
 
 if [ "$1" = "up" ]; then
-    mvn -pl ${SERVICE_NAME}-server -P+layer -am clean package
+    if [ "${2:0:2}" = "-D" ]; then
+        mvn -pl ${SERVICE_NAME}-server -P+layer -am "${2}" clean package
+    else
+        mvn -pl ${SERVICE_NAME}-server -P+layer -am clean package
+    fi
+    docker rmi apzda/${SERVICE_NAME}-server:${SERVICE_VER}
 fi
 
 if [ -z "$1" ]; then
     exec docker compose up
 else
-    exec docker compose "$@"
+    exec docker compose "${@:0:1}"
 fi
